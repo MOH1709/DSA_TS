3ccc61b706344f46f99822fd268b3d22aa469aa0
Priyanshi <ahirwalmohit3216@gmail.com>
2022-12-02 06:18:54 +0530

    test commit

--- App.ts
+++ App.ts
@@ -11,7 +11,7 @@ test1.traverse((val) => {
   console.log(val);
 });
 
-// -------------> Testing Stack List
+// ------------> Testing Stack List
 // let sl1 = new StackList();
 // sl1.push(10);
 // sl1.push(20);

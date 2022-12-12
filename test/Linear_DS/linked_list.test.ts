c2fdcb8917ce7dbc9c9c06ba9f964e8f173e0dd1
Priyanshi <ahirwalmohit3216@gmail.com>
2022-12-12 06:47:31 +0530

    added q and linked list test

---
+++ test/Linear_DS/linked_list.test.ts
@@ -0,0 +1,5 @@
+import { SinglyLL, DoublyLL } from "../../Linear_DS";
+
+describe("", () => {
+  it("", () => {});
+});
---
+++ test/Linear_DS/queue.test.ts
@@ -0,0 +1,5 @@
+import { SimpleQ, CircularQ, DequeQ, PriorityQ } from "../../Linear_DS";
+
+describe("", () => {
+  it("", () => {});
+});
